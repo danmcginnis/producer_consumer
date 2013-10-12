e1c6f9a47d26a2f431339ded72a753e0aa9fc2eb
Dan M McGinnis <mcginnis.dan@gmail.com>
2013-10-12 10:39:41 -0400

    added header file, modified producer function.

--- main.c
+++ main.c
@@ -1,27 +1,25 @@
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
+#include "prod_con.h"
 
-#define MAX_SIZE 32
 
-void print_array(int data[], int size);
-void produce(int data[], int size);
-void consume(int data[], int size);
-
-int main(void)
+int main(int argc, char *argv[])
 {
-    int data[MAX_SIZE];
-    srandom(time(NULL));    //random is preferred over rand
-    int i = 0;
-    for (i = 0; i < 5; i++)
+    
+    if (argc < 2)
     {
-    produce(data, MAX_SIZE);
-    print_array(data, MAX_SIZE);
-    printf("\n\n");
-    consume(data, MAX_SIZE);
-    print_array(data, MAX_SIZE);
-    printf("\n---------------\n");
+    	printf("Please enter the size of the array\n");
+    	exit(1);
     }
+    
+    int temp = atoi(argv[1]);
+    int *full = &temp;      //this hack sucks. There has to be a better way.
+    int size = temp;
+    
+    printf("Size: %d\tFull: %d\n", size, *full);
+    int data[size];
+    srandom(time(NULL));    //random is preferred over rand
+   
+	produce(data, size, full);
+    printf("Size: %d\tFull: %d\n", size, *full);
     return 0;
 }
 
@@ -50,90 +48,16 @@ int main(void)
  *    The current values in data are of no concern.
  *
  */
-void produce(int data[], int size)
+void produce(int data[], int size, int *full)
 {
-
-    int i = 0;
-
-        for (i = MAX_SIZE-1; i > -1; i--)
-        {
-
-            int j = random();
-            j = j % size;
-            data[j] = random();
-            printf("Cell %d was just updated with %d\n", j, data[j]);
-        }
-}
-
-
-/*
- * void consume(int data[], int size)
- *
- * This function accepts an array of integers and 
- *    the size of the array as input. Starting at 
- *    the front of the array cells are filled in
- *    with NULLs.
- *
- * Input:
- *    int data[]: an array of type integer
- *    int size: an integer specifying the size of the
- *        array.
- *
- * Output:
- *    none (should return a int to indicate success or failure)
- *
- * Modifies:
- *    The data[] array is filled with NULLs.
- *    
- * Assumptions:
- *    data[] has already been allocated.
- *    size accuratley represents the size of data.
- *    The current values in data are of no concern.
- *
- */
-void consume(int data[], int size)
-{
-
-    int i = 0;
-
-        for (i = 0; i < MAX_SIZE; i++)
-        {
-
-            data[i] = NULL;
-        }
-}
-
-
-
-/*
- * void print_array(int data[], int size)
- *
- * This function accepts an array of integers and 
- *    the size of the array as input. Starting at 
- *    the front of the array cells are printed to
- *    the screen.
- *
- * Input:
- *    int data[]: an array of type integer
- *    int size: an integer specifying the size of the
- *        array.
- *
- * Output:
- *    The array's content is printed to the screen.
- *
- * Modifies:
- *    none.
- *
- * Assumptions:
- *    data[] has already been allocated.
- *    size accuratley represents the size of data.
- *
- */
-void print_array(int data[], int size)
-{
-    int i = 0;
-    for (i=0; i < size; i++)
+    int i = size;
+    int j = 0;
+    while (*full > 0)
     {
-        printf("|%d|", data[i]);
-    }
+            j = i % size;
+            data[j] = random();
+            i--;
+            *full = (*full) - 1;
+            printf("Cell %d was just updated with %d\n", j, data[j]);
+     } 
 }
---
+++ prod_con.h
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+
+void print_array(int data[], int size);
+void produce(int data[], int size, int *full);
+void consume(int data[], int size, int *full);
+
+
+/*
+ * void consume(int data[], int size, int *full)
+ *
+ * This function accepts an array of integers and 
+ *    the size of the array as input. Starting at 
+ *    the front of the array cells are filled in
+ *    with NULLs.
+ *
+ * Input:
+ *    int data[]: an array of type integer
+ *    int size: an integer specifying the size of the
+ *        array.
+ *
+ * Output:
+ *    none (should return a int to indicate success or failure)
+ *
+ * Modifies:
+ *    The data[] array is filled with NULLs.
+ *    
+ * Assumptions:
+ *    data[] has already been allocated.
+ *    size accuratley represents the size of data.
+ *    The current values in data are of no concern.
+ *
+ */
+void consume(int data[], int size, int *full)
+{
+
+    int i = 0;
+
+        for (i = 0; i < size; i++)
+        {
+
+            data[i] = NULL;
+        }
+}
+
+
+
+/*
+ * void print_array(int data[], int size)
+ *
+ * This function accepts an array of integers and 
+ *    the size of the array as input. Starting at 
+ *    the front of the array cells are printed to
+ *    the screen.
+ *
+ * Input:
+ *    int data[]: an array of type integer
+ *    int size: an integer specifying the size of the
+ *        array.
+ *
+ * Output:
+ *    The array's content is printed to the screen.
+ *
+ * Modifies:
+ *    none.
+ *
+ * Assumptions:
+ *    data[] has already been allocated.
+ *    size accuratley represents the size of data.
+ *
+ */
+void print_array(int data[], int size)
+{
+    int i = 0;
+    for (i=0; i < size; i++)
+    {
+        printf("|%d|", data[i]);
+    }
+    printf("\n\n");
+}
+
+
